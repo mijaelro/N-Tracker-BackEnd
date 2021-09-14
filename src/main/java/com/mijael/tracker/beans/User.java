8b59fba4fc9c27e2b051757e00834db25aa87c30
mijaelro <mijael.rofe@hotmail.com>
2021-09-14 12:06:15 +0300

    2

--- src/main/java/com/mijael/tracker/beans/User.java
+++ src/main/java/com/mijael/tracker/beans/User.java
@@ -1,6 +1,8 @@
 package com.mijael.tracker.beans;
 
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.context.annotation.Scope;
 
 import javax.persistence.*;
@@ -33,14 +35,17 @@ public class User {
     private String password;
 
     @Singular
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @OneToMany(cascade=CascadeType.ALL ,fetch = FetchType.EAGER)
     private List<Expense> expenses;
 
     @Singular
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @OneToMany(cascade = CascadeType.ALL)
     private List<Link> links;
 
     @Singular
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @OneToMany(cascade =CascadeType.ALL)
     private List<Todo> todos;
 
